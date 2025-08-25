ed816b5ed4a59a10d396a848500f6669d219bc79
YuriVanegas30 <yavanegasr@academia.usbbog.edu.co>
2025-08-25 21:36:25 +0000

    Finalizado reto4

--- java_retos/reto4_buscar_strings/Reto4.java
+++ java_retos/reto4_buscar_strings/Reto4.java
@@ -9,9 +9,9 @@ import java.util.regex.*;
 
 public class Reto4 {
     public static void main(String[] args) {
-        String texto = "El mensaje es \"Hola mundo\" y la clave es \"1234\".";
+        String texto = "El mensaje es \"Holiwis\" y la clave es \"1000004\".";
         // Expresión regular para strings entre comillas dobles
-        String patron = "\"(.*?)\"";
+        String patron = "\"(.*?)\""; // funciona bien
         Pattern pattern = Pattern.compile(patron);
         Matcher matcher = pattern.matcher(texto);
         System.out.print("Strings encontrados: ");
